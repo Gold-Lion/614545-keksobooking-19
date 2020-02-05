15fddb27eecb42f421dc2ca93ab08d14c9e40d26
Igor <igor.lavrinoff@yandex.ru>
2020-02-05 20:26:11 +0400

    Реализовал активацию страницы по всем пунктам ТЗ

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 'use strict';
-/*
 
-Перед выполнением задания четвертой лекции, по возможности сделать рефакторинг кода
-
-*/
 var HOUSE_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var CHECKIN_CHECKOUT_TIME = ['12: 00', '13: 00', '14: 00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
@@ -15,6 +11,10 @@ var PHOTO_URLS = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg'
 ];
+var KeyCode = {
+  ESC: 27,
+  ENTER: 13
+};
 var TOTAL_PINS = 8;
 var PIN_WIDTH = 50;
 var MAP_WIDTH = 1200;
@@ -22,18 +22,59 @@ var MIN_X = 0;
 var MAX_X = MAP_WIDTH - PIN_WIDTH;
 var MIN_Y = 130;
 var MAX_Y = 630;
+var isDisabled = true;
 var map = document.querySelector('.map');
 var mapPins = map.querySelector('.map__pins');
+var mapPinMain = map.querySelector('.map__pin--main');
 var mapFilters = map.querySelector('.map__filters-container');
 var templatePin = document.querySelector('#pin').content.querySelector('.map__pin');
 var templateCard = document.querySelector('#card').content.querySelector('.map__card');
+var adForm = document.querySelector('.ad-form');
+var formFilters = mapFilters.querySelector('.map__filters');
+
+var disabledState = function (bool) {
+  adForm.querySelectorAll('fieldset').forEach(function (fieldset) {
+    fieldset.disabled = bool;
+  });
+
+  formFilters.querySelectorAll('select').forEach(function (select) {
+    select.disabled = bool;
+  });
+
+  formFilters.querySelectorAll('fieldset').forEach(function (fieldset) {
+    fieldset.disabled = bool;
+  });
+};
+
+disabledState(isDisabled);
+
+var onPinMainEnterPress = function (evt) {
+  if (evt.keyCode === KeyCode.ENTER) {
+    activeMap();
+  }
+};
 
 var activeMap = function () { // Активируем карту
-  map.classList.remove('map--faded');
+  if (map.classList.contains('map--faded')) {
+    isDisabled = false;
+    map.classList.remove('map--faded');
+    adForm.classList.remove('ad-form--disabled');
+    mapPinMain.removeEventListener('keydown', onPinMainEnterPress);
 
-  renderPin(getArrayAds(TOTAL_PINS));
-  renderCard(getArrayAds(TOTAL_PINS), TOTAL_PINS);
+    disabledState(isDisabled);
+
+    renderPin(getArrayAds(TOTAL_PINS));
+    // renderCard(getArrayAds(TOTAL_PINS), TOTAL_PINS);
+  }
 };
+
+mapPinMain.addEventListener('mousedown', function (evt) {
+  if (evt.button === 0) {
+    activeMap();
+  }
+});
+
+mapPinMain.addEventListener('keydown', onPinMainEnterPress);
 
 var getRandomNumber = function (min, max) { // Получаем случайное число в заданном диапазоне
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -206,5 +247,3 @@ var renderPhotos = function (photos) { // Отрисовываем фотогр�
     photosList.classList.add('hidden');
   }
 };
-
-activeMap();
