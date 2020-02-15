f390e6916d062237784339242c52c0e5521a265c
Igor <igor.lavrinoff@yandex.ru>
2020-02-15 22:44:16 +0400

    Разбил код на модули

---
+++ js/map.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * План на вторую часть
+ * 1) Сделать рефакторинг кода для функции которая блокирует форму  +++
+ * 2) Попробывать переписать валидацию для проверки количества комнат и гостей
+ * 3) Исправить ошибку с неотображением с первого раза данных по photos и features, если не получиться самостоятельно то написать на toster'е и приложить демку
+ * 4) Проверить правильность разбиения кода на модули
+ */
+
+(function () {
+  var map = document.querySelector('.map');
+  var mapPinMain = map.querySelector('.map__pin--main');
+  var adForm = document.querySelector('.ad-form');
+  var address = adForm.querySelector('#address');
+
+  var activeMap = function () { // Активируем карту
+    if (map.classList.contains('map--faded')) {
+      var isActive = true;
+      var isDisabled = false;
+
+      map.classList.remove('map--faded');
+      adForm.classList.remove('ad-form--disabled');
+
+      address.value = window.pin.getCoordPinMain(isActive);
+
+      window.form.disabledState(isDisabled);
+      window.pin.renderPins(window.data.getArrayAds());
+
+      mapPinMain.removeEventListener('keydown', onPinMainEnterPress);
+    }
+  };
+
+  var onPinMainEnterPress = function (evt) {
+    window.util.isEnterEvent(evt, activeMap);
+  };
+
+  var onMapPinMouseDown = function (evt) {
+    window.util.isButtonEvent(evt, activeMap);
+  };
+
+  mapPinMain.addEventListener('keydown', onPinMainEnterPress);
+  mapPinMain.addEventListener('mousedown', onMapPinMouseDown);
+
+})();
